1c85b05521f56c17b9a92e4f595f3d2ceb45bfea
Orim Dominic <orimdominic@gmail.com>
2022-06-28 13:10:53 +0100

    ft: set up authorizer

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
+// TODO-DONE: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl =  process.env.AUTH_0_JWKS_URL
+let cachedCert;
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -54,14 +54,48 @@ export const handler = async (
   }
 }
 
+async function getCertificate(): Promise<string> {
+  if(cachedCert) return cachedCert
+  try {
+    const getJwksResponse = await Axios.get(jwksUrl)
+    const jwks = getJwksResponse.data.keys
+    if (!jwks || !jwks.length) {
+      throw new Error('The JWKS endpoint did not contain any keys');
+    }
+    const signingKeys = jwks
+    .filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signature verification
+                && key.kty === 'RSA' // We are only supporting RSA (RS256)
+                && key.kid           // The `kid` must be present to be useful for later
+                && ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+    ).map(key => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+    });
+    if (!signingKeys.length) {
+      throw new Error('The JWKS endpoint did not contain any signature verification keys');
+    }
+    cachedCert = signingKeys[0].publicKey
+  } catch (error) {
+
+  }
+}
+
+function certToPEM(cert: string) {
+  cachedCert = cert.match(/.{1,64}/g).join('\n');
+  cachedCert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cachedCert;
+}
+
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const cert = await getCertificate()
+  return verify(token, cert, {
+    algorithms: ["RS256"]
+  }) as JwtPayload
 
-  // TODO: Implement token verification
+  // TODO-DONE: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  // return undefined
 }
 
 function getToken(authHeader: string): string {
